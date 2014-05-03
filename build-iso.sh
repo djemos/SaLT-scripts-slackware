26769d9f0cd04d833f3699d5248cd5b2691c1c64
Dimitris Tzemos <djemos@salixos.org>
2014-05-03 22:55:39 +0300

    Update build-iso.sh

--- build-iso.sh
+++ build-iso.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # vim: set syn=sh ft=sh et sw=2 sts=2 ts=2 tw=0:
 # Maintainer: JRD <jrd@salixos.org>
-# Contributors: Shador <shador@salixos.org>, Akuna <akuna@salixos.org>
+# Contributors: Shador <shador@salixos.org>, Akuna <akuna@salixos.org> <djemos@slackel.gr>
 # Licence: GPL v3+
 #
 # Used to build the live ISO
-# build-slackware-live.sh is used to create the initrd need to bootthe Live CD.
+# build-slackware-live.sh (from slackware-live) is used to create the initrd.gz need to boot the Live CD.
 # build-slackware-live.sh is used also to build this Live CD. 
 # funionfs (unionfs with fuse) is still used for building the modules because it is simpler and easier than aufs which may not be compiled in your kernel and which is not available in FUSE. The 0.4.2 version is used in time of writing, instead of 0.4.3 because this one is buggy. Later versions must be checked to see if it's ok with "cp" and "mkfifo" for example.
 #
