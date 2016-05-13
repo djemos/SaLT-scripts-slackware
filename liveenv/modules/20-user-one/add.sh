6216bc8593060590b85ed550cec8b0b8dc0118ef
Dimitris Tzemos <djemos@salixos.org>
2016-05-13 21:34:04 +0300

    Update add.sh

--- liveenv/modules/20-user-one/add.sh
+++ liveenv/modules/20-user-one/add.sh
@@ -30,7 +30,7 @@ EOF
 
 echo 'one:x:1000:100::/home/one:/bin/bash' >> "$RDIR"/etc/passwd
 echo 'one:$5$wC3Ap/4t7$o/lDUbBMF5RT7DFaHYW2g0NO.ArrvHd7G2m1mdHhXR7:13686:0:99999:7:::' >> "$RDIR"/etc/shadow
-sed -i 's/^floppy:.*/\0one/; s/^audio:.*/\0one/; s/^video:.*/\0one/; s/^cdrom:.*/\0one/; s/^plugdev:.*/\0one/; s/^power:.*/\0one/; s/^netdev:.*/\0one/; s/^scanner:.*/\0one/; s/^users:.*/\0one/; s/^fuse:.*/\0one/; s/^wheel:.*/\0,one/;' "$RDIR"/etc/group
+sed -i 's/^floppy:.*/\0one/; s/^audio:.*/\0,one/; s/^video:.*/\0one/; s/^cdrom:.*/\0one/; s/^plugdev:.*/\0one/; s/^polkitd:.*/\0one/; s/^power:.*/\0one/; s/^netdev:.*/\0one/; s/^scanner:.*/\0one/; s/^users:.*/\0one/; s/^fuse:.*/\0one/; s/^wheel:.*/\0,one/;' "$RDIR"/etc/group
 mkdir -p "$RDIR"/home/one
 cp -r "$RDIR"/etc/skel/* "$RDIR"/etc/skel/.??* "$RDIR"/home/one/ 2>/dev/null
 
